194bc7755684ab6789f9a6d4826e4253fc88a954
wanhua1993 <1126923673@qq.com>
2019-08-12 15:45:00 +0800

    测试版本发布

---
+++ build/precache-manifest.2ce226eece79cd1d2854af6efcc24947.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e50ee31e75686cfb34f869a8e8ae4501",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b25c1c5e198d6b53ba3",
+    "url": "/static/css/2.4456446a.chunk.css"
+  },
+  {
+    "revision": "e848a3b6ed08f58b6654",
+    "url": "/static/css/main.f0471652.chunk.css"
+  },
+  {
+    "revision": "6b25c1c5e198d6b53ba3",
+    "url": "/static/js/2.974120f6.chunk.js"
+  },
+  {
+    "revision": "e848a3b6ed08f58b6654",
+    "url": "/static/js/main.6c3b5dc0.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
